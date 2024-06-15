3135b541b64e912629592488b4f9800e301c8105
leobema <leonardoberti21@gmail.com>
2024-06-15 12:30:01 -0300

    añadir mensaje de whastapp

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,17 @@ function App() {
     setAmount(event.target.value);
   };
 
+  const handleSendMessage = () => {
+    const message = `Hola, quiero comprar ${amount} USD con ${selectedCoin}. El monto en ARS es ${amount * ars} ARS.`;
+    const whatsappUrl = `https://wa.me/5492612514163?text=${encodeURIComponent(message)}`;
+    window.open(whatsappUrl, '_blank');
+  };
+
   const calculatedValue = amount ? amount * ars : '';
 
   return (
     <>
-    <section className=" w-full h-[540px] bg-slate-200">
+    <section className=" w-full h-[740px] bg-slate-200">
       <div className="p-10 container mx-auto">
           <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"> 
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">¿Que moneda deseas intercambiar?</h5>            
@@ -59,6 +65,7 @@ function App() {
                       Monto en $ARS a cambiar <span className="text-2xl">${calculatedValue}</span>
                       {' '} y usted recibirá: <span className="text-green-700 text-2xl">{amount}$ USD</span>
                     </p>
+                    <button className=' mt-2 bg-green-400 p-2 border-spacing-2 rounded-md' onClick={handleSendMessage}>Cambiar</button>
                   </div>
                 )}
               </div>
